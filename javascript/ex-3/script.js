aedead21652ea578d4a673b70024ee90852155f5
Ericelmo Augusto <76023768+ericelmoaugusto@users.noreply.github.com>
2020-12-23 08:49:12 +0000

    Add files via upload

---
+++ javascript/ex-3/script.js
@@ -0,0 +1,6 @@
+function calculo() {
+    var nr =  Number(prompt('Digita um número inteiro qualquer'))
+    var n1 = nr - 1 
+    var n2 = nr + 1
+    alert(`Antes de ${nr}, temos o número ${n1}.\nDepois de ${nr}, temos o número ${n2}.`)
+}
