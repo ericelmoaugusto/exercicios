eaf48f2462c8bf7966e059eb805085c8ca8d1150
Ericelmo Augusto <76023768+ericelmoaugusto@users.noreply.github.com>
2020-12-23 08:39:40 +0000

    Add files via upload

---
+++ javascript/ex-2/script.js
@@ -0,0 +1,5 @@
+function pgts() {
+    var nome = prompt('Como te chamas?')
+    var idade = prompt(`Olá, ${nome}! Tens quantos anos?`)
+    var final = alert(`Acabei de conhecer o ${nome}, que tem ${idade} anos de idade!`)
+}
