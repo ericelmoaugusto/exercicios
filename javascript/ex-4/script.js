fc5eabd6dd05511441b67037f258ddaba03637cc
Ericelmo Augusto <76023768+ericelmoaugusto@users.noreply.github.com>
2020-12-23 22:13:10 +0000

    Update script.js

--- javascript/ex-4/script.js
+++ javascript/ex-4/script.js
@@ -1,14 +1,13 @@
 function calculo() {
-    produto = window.prompt('Que produto estás a comprar?')
-    preco = window.prompt(`Quanto custa o(a) ${produto} que estás a comprar?`)
-    pag = window.prompt(`Quanto dinheiro entregaste para pagar o(a) ${produto}?`)
+    produto = prompt('Que produto estás a comprar?')
+    preco = Number(prompt(`Quanto custa o(a) ${produto} que estás a comprar?`))
+    pag = Number(prompt(`Quanto dinheiro entregaste para pagar o(a) ${produto}?`))
     troco1 = pag - preco
     falta = -(pag - preco)
     
     if (pag >= preco) {
-        window.prompt(`Compraste um(a) ${produto} que custou €${preco}.\nDeste €${pag} em dinheiro e vais receber €${troco1} de troco.\nObrigado e até já!`)
-    }
-    else {
-        window.alert(`O/A ${produto} custa €${preco} e tu entregaste €${pag}.\nFalta €${falta} para concluir a tua compra.\nObrigado.`)
+        alert(`Compraste um(a) ${produto} que custou €${preco}.\nDeste €${pag} em dinheiro e vais receber €${troco1} de troco.\nObrigado e até já!`)
+    } else {
+        alert(`O/A ${produto} custa €${preco} e tu entregaste €${pag}.\nFalta €${falta} para concluir a tua compra.\nObrigado.`)
     }
 }
