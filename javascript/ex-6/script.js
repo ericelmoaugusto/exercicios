cd35d0f3db870eea68fa0f556fc7e566bb0fcdee
Ericelmo Augusto <76023768+ericelmoaugusto@users.noreply.github.com>
2020-12-25 13:56:03 +0000

    Update script.js

--- javascript/ex-6/script.js
+++ javascript/ex-6/script.js
@@ -3,14 +3,14 @@ function calculo () {
     let kel = (cel + 273.15).toLocaleString("pt-BR")
     let far = ((cel * 1.8) + 32).toLocaleString("pt-BR")
     
-    //converter para formato de °C (Celsius)
+    //converter formato de °C (Celsius)
     let cel1 = cel.toLocaleString("pt-BR")
 
-    var str = `A temperatura de ${cel1}°C, corresponde a...`
+    var str = `A temperatura de ${cel1} °C, corresponde a...`
     var res = str.bold().fontsize(5)
     document.getElementById("msg").innerHTML = res;
 
     msg.innerHTML += `
-    <p>${kel}°K (Kelvin)<p>
-    <p>${far}°F (Fahrenheit)<p>`
+    <p>${kel} °K (Kelvin)<p>
+    <p>${far} °F (Fahrenheit)<p>`
 }
